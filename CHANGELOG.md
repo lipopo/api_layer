40eef2121b98727aa51ad7d3ed150ad83425d928
lipo <lipo8081@gmail.com>
2020-11-06 12:55:50 +0800

    Add Changelog and Change Version

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# ApiLayer
+
+## v0.0.1
+### Added
+
+- Setup Api Struct
+
+## v0.0.1
+### Added
+
+- tencent v3 auth
+- tencent update_function_code action
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "rb") as f:
 
 setup(
     name="ApiLayer",
-    version="0.0.1",
+    version="0.0.2",
     author="lipo",
     author_email="lipo8081@gmail.com",
     description="Basic Api Layer",
